0ec4b096cdb93846db7a9574db6290bf6411f6b1
magamlag <lagovskiy@gmail.com>
2016-11-16 16:33:26 -0500

    Add routes for Tags

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,13 @@ use Illuminate\Http\Request;
 */
 
 Route::resource('/notes', 'NotesController');
+
+Route::post('/notes/{id}/add_tag',[
+    'as'    =>  'api.notes.add_tag',
+    'uses'  =>  'NotesController@addTag'
+]);
+
+Route::delete('/notes/{note_id}/remove_tag/{tag_id}',[
+    'as'    =>  'api.notes.remove_tag',
+    'uses'  =>  'NotesController@removeTag'
+]);
