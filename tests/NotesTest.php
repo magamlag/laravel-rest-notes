b84f20f18d3718be2d4c3925a271a286d7d46d0c
magamlag <lagovskiy@gmail.com>
2016-11-16 16:33:09 -0500

    Add test for Tags

--- tests/NotesTest.php
+++ tests/NotesTest.php
@@ -6,9 +6,67 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class NotesTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /**
-     * A basic test example.
-     *
+     * @return void
+     */
+    public function testAddNoteTag()
+    {
+        $this->post('/api/notes/1/add_tag?api_token=1',[
+            'tag'   =>  'testcasetag'
+        ])->seeStatusCode(200)->seeJsonContains([
+           'success'    =>  true,
+        ]);
+    }
+
+    /**
+     * @return void
+     */
+    public function testAddNoteTagBadWithNoteNotFound()
+    {
+        $this->post('/api/notes/133333/add_tag?api_token=1',[
+            'tag'   =>  'testcasetag'
+        ])->seeStatusCode(404)->seeJsonContains([
+            'success'    =>  false,
+        ]);
+    }
+
+    /**
+     * @return void
+     */
+    public function testAddNoteTagBadWithEmptyTag()
+    {
+        $this->post('/api/notes/1/add_tag?api_token=1')
+            ->seeStatusCode(422)->seeJsonContains([
+            'success'    =>  false,
+        ]);
+    }
+
+
+    /**
+     * @return void
+     */
+    public function testRemoveNoteTag()
+    {
+        $this->delete('/api/notes/1/remove_tag/1?api_token=1')
+            ->seeStatusCode(200)->seeJsonContains([
+                'success'    =>  true,
+            ]);
+    }
+
+    /**
+     * @return void
+     */
+    public function testRemoveTagBadNoteNotFound()
+    {
+        $this->delete('/api/notes/14444444/remove_tag/1?api_token=1')
+            ->seeStatusCode(404)->seeJsonContains([
+                'success'    =>  false,
+            ]);
+    }
+
+    /**
      * @return void
      */
     public function testShowAllNotes()
@@ -17,6 +75,9 @@ class NotesTest extends TestCase
             ->seeStatusCode(200);
     }
 
+    /**
+     * @return void
+     */
     public function testAddNote()
     {
         $this->post('/api/notes/?api_token=1', [
@@ -26,6 +87,9 @@ class NotesTest extends TestCase
         ]);
     }
 
+    /**
+     * @return void
+     */
     public function testAddEmptyNote()
     {
         $this->post('/api/notes/?api_token=1')->seeJsonContains([
@@ -33,6 +97,9 @@ class NotesTest extends TestCase
         ])->seeStatusCode(422);
     }
 
+    /**
+     * @return void
+     */
     public function testNoteEdit()
     {
         $this->put('/api/notes/1/?api_token=1',[
@@ -42,6 +109,9 @@ class NotesTest extends TestCase
         ]);
     }
 
+    /**
+     * @return void
+     */
     public function testNoteEditWithEmptyText()
     {
         $this->put('/api/notes/1/?api_token=1')->seeStatusCode(422)->seeJsonContains([
@@ -49,6 +119,9 @@ class NotesTest extends TestCase
         ]);
     }
 
+    /**
+     * @return void
+     */
     public function testNoteDelete()
     {
         $this->delete('/api/notes/1/?api_token=1')
@@ -58,6 +131,9 @@ class NotesTest extends TestCase
             ]);
     }
 
+    /**
+     * @return void
+     */
     public function testNoteDeleteNotFound()
     {
         $this->delete('/api/notes/99999/?api_token=1')
