e8a3c1a68c00dae1863830ac7e480b55b6dfc304
andrew <lagovskiy@gmail.com>
2017-04-29 16:13:02 -0400

    Create login form

---
+++ resources/views/github/login.blade.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>GITHUB Login Form</title>
+    </head>
+    <body>
+        <form action="{{route('github.postlogin')}}" method = "POST">
+            {{csrf_field()}}
+            <b>Login : </b><br>
+            <input type="text" name = "login" value = "<?php env('GITHUB_USER'); ?>">
+            <br>
+            <b>Password : </b><br>
+            <input type="password" name = "password" value = "<?php env('GITHUB_PASSWORD'); ?>">
+            <br>
+            <input type="submit" value = "log in">
+        </form>
+    </body>
+</html>
