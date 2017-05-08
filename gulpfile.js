ecb798640fba1f647040e8c53d0ed12ac9bd71c8
andrew <lagovskiy@gmail.com>
2017-05-08 12:34:53 -0400

    Copy fonts to /build/fonts folder using mix.copy in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 const elixir = require('laravel-elixir');
 
 require('laravel-elixir-vue-2');
-require('laravel-elixir-webpack-official');
+require('laravel-elixir-webpack');
+
+//Override default settings and makes command "gulp watch" works
+/*
+elixir.config.js.webpack.watchify = {
+	enabled: true,
+	options: {
+		poll: true
+	}
+}
+*/
+
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -13,15 +24,24 @@ require('laravel-elixir-webpack-official');
  |
  */
 
-elixir(function(mix) {
-    mix.sass('app.scss')
-       .webpack('main.js');
+elixir(function (mix) {
+	mix.sass('app.scss')
+			.webpack('main.js');
 });
 
 /* Combine and minify styles for GitHub User Dashboard */
-elixir(function(mix) {
+elixir(function (mix) {
 	mix.styles([
-		'normalize.css',
-		'login-form.css'
-	], 'public/css/github.css');
+		'/github-web/bootstrap.min.css',
+		'/github-web/github-dash.css',
+		'/github-web/font-awesome.min.css',
+	], 'public/css/github-dash.css');
+	mix.copy('resources/assets/css/fonts', 'public/build/fonts');
+	mix.scripts([
+		'/github-web/jquery.js',
+		'/github-web/bootstrap.js',
+		'/github-web/github-dash.js'
+	], 'public/js/github-dash.js');
+	mix.version(['css/github-dash.css','js/github-dash.js']);
 });
+
