246b30880061ebddd10a5dcf3c1f947edd49d13c
andrew <lagovskiy@gmail.com>
2017-05-09 09:41:07 -0400

    Remove 'area' template. Generated style

---
+++ resources/assets/js/github-web/github-dash.js
@@ -0,0 +1,24 @@
+/*
+ $('#tabs').on('click', '.tablink, #prodTabs a', function (e) {
+ e.preventDefault();
+ var url = $(this).attr("data-url");
+
+ if (typeof url !== "undefined") {
+ var pane = $(this), href = this.hash;
+
+ // ajax load from data-url
+ $(href).load(url, function (result) {
+ pane.tab('show');
+ });
+ } else {
+ $(this).tab('show');
+ }
+ });
+ */
+/*
+$(document).ready(function () {
+	$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+//show selected tab / active
+		console.log($(e.target).attr('id'));
+	});
+})*/
