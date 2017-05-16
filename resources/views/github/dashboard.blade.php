d6fd9b83aa97a6cf98e36079f7dd29153982c271
andrew <lagovskiy@gmail.com>
2017-05-16 18:22:35 -0400

    Fix height and width of columns in view

--- resources/views/github/dashboard.blade.php
+++ resources/views/github/dashboard.blade.php
@@ -13,8 +13,7 @@
         <div class="col-lg-12">
             <h1 class="page-header text-center">Github User Area</h1>
         </div>
-    </div>
-    <!-- /.row -->
+    </div><!-- /row -->
     <div class="row" id="area">
         <ul class="nav nav-pills" role="tablist">
             <li class="active">
@@ -28,43 +27,45 @@
         <div class="tab-content">
             <div class="tab-pane active" id="repos" role="tabpanel">
                 <h3>Content's background color is the same for the tab</h3>
+                <?php $key = 1; ?>
                 @if($repos)
-                    @foreach($repos as $key => $repo)
-                        @if($key != 0 && $key % 4 == 0)
-                            <div class="row row-eq-height row-eq-width">
-                        @endif
-                                <div class="col-md-4 col-sm-6 repo-col">
-                                    <div class="repo-item">
-                                        <div class="pull-right stars">
-                                            <i class="fa fa-star" aria-hidden="true"></i>
-                                            @php echo $repo->stargazers_count ? $repo->stargazers_count : ''  @endphp
-                                        </div>
-                                        <a href="{{$repo->html_url}}">to Github</a>
-                                        <h3>
-                                            <a href="#">{{$repo->name}}</a>
-                                        </h3>
-                                        <time>{{$repo->updated_at}}</time>
-                                        <div class="last-commit">
-
-                                        </div>
-                                        <div class="repo-item-text">
-                                            {{$repo->description}}
-                                        </div>
+                    @foreach(array_chunk($repos,3) as $items )
+                        <div class="row same-size">
+                        @foreach($items as $k => $repo)
+                            <div class="col-md-4 col-sm-6 repo-col">
+                                <div class="repo-item">
+                                    <div class="pull-right stars">
+                                        <i class="fa fa-star" aria-hidden="true"></i>
+                                        @php echo $repo->stargazers_count ? $repo->stargazers_count : ''  @endphp
                                     </div>
+                                    <a href="{{$repo->html_url}}">to Github</a>
+                                    <h3>
+                                        <a href="#">{{$repo->name}}</a>
+                                    </h3>
+                                    <time>{{$repo->updated_at}}</time>
+                                    <div class="last-commit">
+
+                                    </div>
+                                    <div class="repo-item-text">
+                                        @if($k % 3 == 0)
+                                        There are plenty of articles that explain what design patterns are, and how to implement them; the web doesn't need yet another one of those articles! Instead, in this article, we will more discuss the when and why, rather than the which and how.
+                                        @else
+                                        Use a Factory Pattern when you find yourself writing code to gather information necessary to create objects.
+                                            @endif
+                                    </div>
+
                                 </div>
-                        @if($key != 0 && $key % 4 == 0)
-                            </div><!-- /row -->
-                        @endif
+                            </div>
+                        @endforeach
+                        </div>
                     @endforeach
                 @else
                     No repositories
                 @endif
-            </div>
-            <div class="tab-pane active" id="issues" role="tabpanel">
-
-            </div>
+            </div><!-- /tab-pane -->
+            <div class="tab-pane active" id="issues" role="tabpanel"></div><!-- /tab-pane -->
         </div><!-- /tab-content -->
-    </div>
+    </div><!-- /row -->
 </div>
 </body>
 <script></script>
